057a3296ad478b8c33a29a52493e87ce0b98fb4c
PatelManav <manav.p@ahduni.edu.in>
2020-07-19 11:57:19 +0530

    questions fetched from the API of trivia quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return <div className='App'>React Quiz</div>
+  const [number, setNumber] = useState(0)
+  const [question, setQuestion] = useState([])
+  const [userAnswers, setuserAnswers] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [gameOver, setgameOver] = useState(true)
+  const [score, setScore] = useState(0)
+  const startTrivia = () => {}
+  const checkAnswer = () => {}
+  const nextQuestion = () => {}
+  return (
+    <div className='app'>
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        start
+      </button>
+      <p className='score'>Score: </p>
+      <p>Loading questions... </p>
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  )
 }
 
 export default App
